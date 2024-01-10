facf63b685b91f6591baee371d139970a8430682
zaibachkhoa <nguyenlamvu123@gmail.com>
2024-01-10 10:39:09 +0700

    - độ chính xác tốt với ảnh trong bài viết digit.jpg nhưng không đúng với các ảnh còn lại, cần debug/code lại phần tiền xử lí

---
+++ thorpham_dhn.py
@@ -0,0 +1,63 @@
+#site: https://thorpham.github.io/blog/Nh%E1%BA%ADn-d%E1%BA%A1ng-ch%E1%BB%AF-s%E1%BB%91-vi%E1%BA%BFt-tay
+import cv2
+import numpy as np
+from skimage.feature import hog
+from sklearn.svm import LinearSVC
+from keras.datasets import mnist
+from sklearn.metrics import accuracy_score
+#load data
+(X_train,y_train),(X_test,y_test) = mnist.load_data()
+
+
+#cho x_train
+X_train_feature = []
+for i in range(len(X_train)):
+    feature = hog(X_train[i],orientations=9,pixels_per_cell=(14,14),cells_per_block=(1,1),block_norm="L2")
+    X_train_feature.append(feature)
+X_train_feature = np.array(X_train_feature,dtype = np.float32)
+
+#cho x_test
+X_test_feature = []
+for i in range(len(X_test)):
+    feature = hog(X_test[i],orientations=9,pixels_per_cell=(14,14),cells_per_block=(1,1),block_norm="L2")
+    X_test_feature.append(feature)
+X_test_feature = np.array(X_test_feature,dtype=np.float32)
+
+
+model = LinearSVC(C=10)
+model.fit(X_train_feature,y_train)
+y_pre = model.predict(X_test_feature)
+print(accuracy_score(y_test,y_pre))
+
+for img in (
+        # 'photo_6118490984477211115_y.jpg',
+        # 'photo_6118490984477211116_y.jpg',
+        # 'photo_6118490984477211117_y.jpg',
+        # 'photo_6118490984477211118_y.jpg',
+        # 'photo_6118490984477211119_y.jpg',
+        # 'photo_6118490984477211120 _y.jpg',
+        'photo_6118490984477211121 _y.jpg',
+        # "digit.jpg",
+):
+    image = cv2.imread(img)
+    im_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    im_blur = cv2.GaussianBlur(im_gray, (5, 5), 0)
+    im, thre = cv2.threshold(im_blur, 90, 255, cv2.THRESH_BINARY_INV)
+    contours, hierachy = cv2.findContours(thre, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    rects = [cv2.boundingRect(cnt) for cnt in contours]
+    for i in contours:
+        (x, y, w, h) = cv2.boundingRect(i)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        roi = thre[y:y + h, x:x + w]
+        roi = np.pad(roi, (20, 20), 'constant', constant_values=(0, 0))
+        roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+        roi = cv2.dilate(roi, (3, 3))
+
+        # Calculate the HOG features
+        roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), block_norm="L2")
+        nbr = model.predict(np.array([roi_hog_fd], np.float32))
+        cv2.putText(image, str(int(nbr[0])), (x, y), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+    cv2.imshow("image", image)
+    # cv2.imwrite("image_pand.jpg",image)
+    cv2.waitKey()
+cv2.destroyAllWindows()
